5dd92cebf7446207c151eec28d0c8a36043bd98a
fhy <fhy17@outlook.com>
2023-06-05 16:10:18 +0800

    feat(map_test): map test

--- cpp11/map_test/map_test.cpp
+++ cpp11/map_test/map_test.cpp
@@ -25,27 +25,35 @@ void mapTest() {
     return;
 }
 
-int returnZero() {
-	return 0;
-}
+int returnZero() { return 0; }
 
 char* returnNullptr() {
-	//return NULL;
-	return nullptr;
+    // return NULL;
+    return nullptr;
 }
 
 void mapString() {
     std::map<int, std::string> mm;
-	//mm[0] = 0; // compile error
+    // mm[0] = 0; // compile error
     mm[0] = returnZero();
-    //mm[1] = returnNullptr(); // memory crash
-	auto s = mm[0];
-	//s = mm[1];
+    // mm[1] = returnNullptr(); // memory crash
+    auto s = mm[0];
+    // s = mm[1];
     return;
 }
+void intTest() {
+    std::map<int, std::string> mm;
+    mm[0] = "0";
+    mm[1] = "1";
+    mm[2] = "2";
 
+    auto it = mm.begin();
+    auto x = it->second;
+    auto y = x;
+}
 int main() {
     mapTest();
     mapString();
+    intTest();
     return 0;
 }
